93fb55c9479c3d111086a076ebdb5fdaf37284ee
Maulana Seto <126048871+MaulanaSeto@users.noreply.github.com>
2023-11-20 20:27:53 +0700

    Update asset_card.dart

--- lib/widgets/asset_card.dart
+++ lib/widgets/asset_card.dart
@@ -4,6 +4,7 @@ import 'package:aset_perusahaan_kereta/asset_form.dart';
 class ButtonItem {
   final String name;
   final IconData icon;
+  final Integer number;
 
-  ButtonItem(this.name, this.icon);
+  ButtonItem(this.name, this.icon, this.number);
 }
